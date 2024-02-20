7b3408afad26b7202948e4f0bd4842637794828f
Woo <97427612+xlnt415@users.noreply.github.com>
2024-02-20 17:54:42 +0900

    Title: 동전 더하기, Time: 159ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240220/동전 더하기/add-coins.py
@@ -0,0 +1,13 @@
+n, k = tuple(map(int, input().split()))
+coins = [
+    int(input())
+    for _ in range(n)
+]
+ans = 0
+
+for coin in coins[::-1]:
+    ans += k // coin
+    k %= coin
+
+
+print(ans)
